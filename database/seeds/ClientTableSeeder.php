1a456d63e21f200c4e8a6405b476978e03886226
Andre Monteiro <andre@Andres-MacBook-Pro.local>
2017-06-14 00:34:53 +0100

    Update Seed to 100 fake users

--- database/seeds/ClientTableSeeder.php
+++ database/seeds/ClientTableSeeder.php
@@ -12,6 +12,6 @@ class ClientTableSeeder extends Seeder
     public function run()
     {
         \CodeProject\Client::truncate();
-        factory(\CodeProject\Client::class, 10)->create();
+        factory(\CodeProject\Client::class, 100)->create();
     }
 }
